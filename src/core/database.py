2a7b0fcba3ab8fe123af47f0e00f418fb740a33a
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2025-03-01 13:43:58 +0300

    Updated DB

---
+++ src/core/database.py
@@ -0,0 +1,13 @@
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from src.core.config import settings
+
+
+async def init_database():
+    client = AsyncIOMotorClient(
+        f"mongodb://{settings.MONGO_INITDB_ROOT_USERNAME}:{settings.MONGO_INITDB_ROOT_PASSWORD}@{settings.MONGODB_HOST}",
+        connectTimeoutMS=5000,
+        timeoutms=5000,
+    )
+    await init_beanie(database=client.besthack, document_models=[])
--- src/main.py
+++ src/main.py
@@ -1,11 +1,32 @@
+import logging
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from src.core.config import settings
+from src.core.database import init_database
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(lineno)d - %(levelname)s - %(message)s",
+)
+
+logger = logging.getLogger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    logger.info("Initializing database...")
+    await init_database()
+    logger.info("Database initialized")
+    yield
+
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     description=settings.PROJECT_DESCRIPTION,
     version=settings.PROJECT_VERSION,
+    lifespan=lifespan,
 )
 
 
