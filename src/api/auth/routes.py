c8413eb1e953cf5bcc4c14ec8f1bf2730ba4457f
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2025-03-02 08:05:14 +0300

    added back validate

--- src/api/auth/routes.py
+++ src/api/auth/routes.py
@@ -2,8 +2,8 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, Response
 
-from src.api.auth.dependencies import RefreshTokenUserGetter, validate_credentials
-from src.api.auth.schemas import TokenInfo
+from src.api.auth.dependencies import RefreshTokenUserGetter, get_current_token_payload, validate_credentials
+from src.api.auth.schemas import JWTData, TokenInfo
 from src.api.auth.service import AuthService
 from src.core.models import User
 
@@ -13,6 +13,11 @@ router = APIRouter(prefix="/auth", tags=["auth"])
 @router.post("/generate", response_model=TokenInfo)
 def generate_tokens(user: Annotated[User, Depends(validate_credentials)], response: Response):
     return AuthService.generate_tokens(user)
+
+
+@router.post("/validate", response_model=JWTData)
+def validate_token(payload: Annotated[JWTData, Depends(get_current_token_payload)]):
+    return payload
 
 
 @router.post("/refresh", response_model=TokenInfo)
